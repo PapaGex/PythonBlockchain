9a63d02fdd7a5d2dd253b2bc16994a4e037f0f4b
PapaGex <cryptoblockchainjunkie@gmail.com>
2021-08-01 20:39:07 -0400

    initiate mining

--- block.py
+++ block.py
@@ -1,19 +1,50 @@
+import time
+
+
+
 class Block:
     """
     Unit of storage
     storing transactions in the blockchain
     """
-    def __init__(self, data):
+    def __init__(self, timestamp, last_hash, hash, data):
+        self.timestamp = timestamp
+        self.last_hash = last_hash
+        self.hash = hash
         self.data = data
 
     def __repr__(self):
-        return f'Block - data: {self.data}' 
+        return (
+            'Block('
+            f'timestamp: {self.timestamp}, '
+            f'last_hash: {self.last_hash}, '
+            f'hash: {self.hash}, '
+            f'data: {self.data})'
+        ) 
+
+    @staticmethod  
+    def mine_block(last_block, data):
+        """
+        mines a block based on the given last_block and data
+        """ 
+        timestamp = time.time_ns()
+        last_hash = last_block.hash
+        hash = f'{timestamp}-{last_hash}'
+
+        return Block(timestamp, last_hash, hash, data)
+    
+    @staticmethod
+    def genesis():
+        """
+        Generate the genesis block
+        """
+        return Block(1, 'genesis_last_hash', 'genesis_hash', []) 
 
 def main():
 
-    block = Block('blood')        
+    genesis_block = Block.genesis()
+    block = Block.mine_block(genesis_block, 'blood')
     print(block)
-    print(f'block.py __name__: {__name__}')
 
 if __name__ == '__main__':
     main()    
