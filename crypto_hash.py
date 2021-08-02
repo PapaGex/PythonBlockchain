6506b7414b9a6f38506f12b025d601411f1b408e
PapaGex <cryptoblockchainjunkie@gmail.com>
2021-08-01 23:14:20 -0400

    improved hashing algorythm and added it to block

--- crypto_hash.py
+++ crypto_hash.py
@@ -1,16 +1,19 @@
 import hashlib
 import json
 
-def crypto_hash(data):
+def crypto_hash(*args):
     """
-    returns a sha-256 hash of the given data
+    returns a sha-256 hash of the given argument
     """
-    stringified_data = json.dumps(data)
+    stringified_args = sorted(map(lambda data: json.dumps(data), args))
 
-    return hashlib.sha256(stringified_data.encode('utf-8')).hexdigest()
+    joined_data = ''.join(stringified_args)
 
+    return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
+   
 def main():
-    print(f"crypto_hash('blood'): {crypto_hash('blood')}")
+    print(f"crypto_hash('blood', 7, [42]): {crypto_hash('blood', 7, [42])}")
+    print(f"crypto_hash(7, [42], 'blood'): {crypto_hash(7, [42], 'blood')}")
 
 if __name__ == '__main__':
     main()    
