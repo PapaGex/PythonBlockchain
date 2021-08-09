1eaa2d67a53e82388b35b8eff993752a5daff833
kawekaweau <cryptoblockchainjunkie@gmail.com>
2021-08-09 08:03:43 -0400

    added proof of work ystem

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,12 +1,15 @@
 import time
 
 from backend.util.crypto_hash import crypto_hash
+from backend.config import MINE_RATE
 
 GENESIS_DATA = {
     'timestamp': 1,
     'last_hash': 'genesis_last_hash',
     'hash': 'genesis_hash',
-    'data': []
+    'data': [],
+    'difficulty': 3,
+    'nonce': 'genesis_nonce'
 }
 
 class Block:
@@ -14,11 +17,13 @@ class Block:
     Unit of storage
     storing transactions in the blockchain
     """
-    def __init__(self, timestamp, last_hash, hash, data):
+    def __init__(self, timestamp, last_hash, hash, data, difficulty, nonce):
         self.timestamp = timestamp
         self.last_hash = last_hash
         self.hash = hash
         self.data = data
+        self.difficulty = difficulty
+        self.nonce = nonce
 
     def __repr__(self):
         return (
@@ -26,32 +31,53 @@ class Block:
             f'timestamp: {self.timestamp}, '
             f'last_hash: {self.last_hash}, '
             f'hash: {self.hash}, '
-            f'data: {self.data})'
+            f'data: {self.data}, '
+            f'difficulty: {self.difficulty}, '
+            f'nonce: {self.nonce})'
         ) 
 
     @staticmethod  
     def mine_block(last_block, data):
         """
-        mines a block based on the given last_block and data
+        mines a block based on the given last_block and data, until a block hash
+        is found that meets the leading 0's POW requirement
         """ 
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        hash = crypto_hash(timestamp, last_hash, data)
+        difficulty = Block.adjust_difficulty(last_block, timestamp)
+        nonce = 0
+        hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce) 
+           
+        while hash[0:difficulty] != '0' * difficulty:
+            nonce += 1
+            timestamp = time.time_ns()
+            difficulty = Block.adjust_difficulty(last_block, timestamp)
+            hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
-        return Block(timestamp, last_hash, hash, data)
+        return Block(timestamp, last_hash, hash, data, difficulty, nonce)
     
     @staticmethod
     def genesis():
         """
         Generate the genesis block
         """
- #       return Block(
- #           GENESIS_DATA['timestamp'],
- #           GENESIS_DATA['last_hash'],
- #           GENESIS_DATA['hash'],
- #           GENESIS_DATA['data'] 
- #           ) 
+
         return Block(**GENESIS_DATA)
+
+    @staticmethod
+    def adjust_difficulty(last_block, new_timestamp):
+        """
+        calculate adjusted difficulty according to MINE_RATE
+        increase if quick
+        decrease if slow
+        """
+        if (new_timestamp - last_block.timestamp) < MINE_RATE:
+            return last_block.difficulty + 1
+
+        if (last_block.difficulty -1) > 0:
+            return last_block.difficulty - 1
+
+        return 1
 
 def main():
 
--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -1,4 +1,7 @@
+import time
+
 from backend.blockchain.block import Block, GENESIS_DATA
+from backend.config import MINE_RATE, SECONDS
 
 def test_mine_block():
     last_block = Block.genesis()
@@ -8,6 +11,7 @@ def test_mine_block():
     assert isinstance(block, Block)
     assert block.data == data
     assert block.last_hash == last_block.hash
+    assert block.hash[0:block.difficulty] == '0' * block.difficulty
 
 def test_genesis():
     genesis = Block.genesis()
@@ -16,3 +20,31 @@ def test_genesis():
 
     for key, value in GENESIS_DATA.items():
         getattr(genesis, key) == value
+
+def test_quickly_mined_block():
+    last_block = Block.mine_block(Block.genesis(), 'foo')
+    mined_block = Block.mine_block(last_block, 'bar')
+
+    assert mined_block.difficulty == last_block.difficulty + 1
+
+def test_slowly_mined_block():
+    last_block = Block.mine_block(Block.genesis(), 'foo')
+    time.sleep(MINE_RATE / SECONDS)
+    mined_block = Block.mine_block(last_block, 'bar')
+
+    assert mined_block.difficulty == last_block.difficulty - 1
+
+def test_mined_block_difficulty_limits_at_1():
+    last_block = Block(
+        time.time_ns(),
+        'test_last_hash',
+        'test_hash',
+        'test_data',
+        1,
+        0
+    )
+
+    time.sleep(MINE_RATE / SECONDS)
+    mined_block = Block.mine_block(last_block, 'bar')
+
+    assert mined_block.difficulty == 1
