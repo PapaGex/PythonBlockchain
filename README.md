c6d316539a455c1b410b7d962ddf20755347b8a9
kawekaweau <cryptoblockchainjunkie@gmail.com>
2021-08-09 02:43:14 -0400

    added tests and a virtual enviroment

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-PythonBlockchain
+PythonBlockchain--  almost verbatim fromn the udemy course build a blockchain and cryptocurrency. after complete modifications will occur.
+
+**The Virtual Enviroment for this project is py3Blockchain-venv**
+```
+source py3Blockchain-venv/bin/activate
+```
+**Install all packages**
+```
+pip3 install -r requirements.txt
+```
+**Run the Tests**
+activate the virtual enviroment first
+
+```
+python3 -m pytest backend/tests
+```
---
+++ backend/blockchain/blockchain.py
@@ -0,0 +1,31 @@
+from backend.blockchain.block import Block
+
+class Blockchain:
+    """
+    Blockchain is a public ledgers of transactions.
+    Implemented as a list of blocks - data sets of transactions
+    """
+    def __init__(self):
+        self.chain = [Block.genesis()]
+
+    def add_block(self, data):
+        self.chain.append(Block.mine_block(self.chain[-1], data))
+
+    def __repr__(self):
+        return f'Blockchain: {self.chain}'
+
+def main():
+    blockchain = Blockchain()
+    blockchain.add_block('one')
+    blockchain.add_block('two')
+    blockchain.add_block('three')
+    blockchain.add_block('four')
+    blockchain.add_block('five')
+    blockchain.add_block('six')
+    blockchain.add_block('seven')
+
+    print(blockchain)
+    print(f'blockchain.py __name__: {__name__}')
+
+if __name__ == '__main__':
+    main()
